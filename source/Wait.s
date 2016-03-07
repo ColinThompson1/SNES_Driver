89e90b574bcd0abc2f9c03d8085e565fd3af0316
lillianiwaniszyn <lillian.iwaniszyn@gmail.com>
2016-03-07 16:32:58 -0700

    r0 and r6 changed

--- source/Wait.s
+++ source/Wait.s
@@ -4,17 +4,13 @@
 
 .globl Wait
 
-//r0: input parameter <-- time to wait
 Wait:
 	mov	r3, r0			//put time parameter in r3
-
-	ldr 	r0, =0x20003004 	// address of CLO
-	ldr 	r1, [r0] 		// read CLO
-	add 	r1, r3			// add amount of time to wait
-waitLoop:
-	ldr 	r2, [r0]		//read CLO
-	cmp 	r1, r2 			// stop when CLO = r1
-	bhi 	waitLoop
-	
-
-	bx	lr
+	ldr r0, =0x20003004 // address of CLO 
+	ldr r1, [r0] // read CLO 
+	add r1, r3 // add time passed in r6 
+WaitLoop: 
+	ldr r2, [r0] 
+	cmp r1, r2 // stop when CLO = r1 
+	bhi WaitLoop 
+	bx lr
