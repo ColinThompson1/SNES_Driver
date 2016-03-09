66a5af8ff0d4bc2ef0e83de844de45de5be1ae5f
lillianiwaniszyn <lillian.iwaniszyn@gmail.com>
2016-03-09 15:24:11 -0700

    Update main.s

--- source/main.s
+++ source/main.s
@@ -15,6 +15,43 @@ main:
     mov     sp, #0x8000
 
 	bl		EnableJTAG
+	bl 		Read_SNES
+	
+	// Print names of creators 
+	mov 	r0, #13 		
+	bl 		Print_Message
+
+Begin:
+
+	// Prompt for button press
+	mov		r0,	#14
+	bl 		Print_Message
+	
+	// Check for checked buttons. Bitmap returned in r0
+	bl		Read_SNES
+	
+	// Initiate counter to 0 and mask to left most bit and the bitmap to r7
+	mov		r4, #0
+	mov 	r5, #1
+	mov		r7, r0
+	Loop:
+		and		r6, r5, r7
+		cmp		r6, r5
+		bne		fail
+		
+		mov		r0, r4
+		bl		Print_Message
+		
+		
+		fail:
+		
+		rsl	r7, #1
+		add	r4, #1 //inc
+		cmp r4, #16
+		bge Begin
+		
+	
+
 
 haltLoop$:
 	b		haltLoop$
