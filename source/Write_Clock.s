4ba1822c3730f804eda919783cdae3ca5779e33f
lillianiwaniszyn <lillian.iwaniszyn@gmail.com>
2016-03-09 20:04:56 -0700

    Update Write_Clock.s

--- source/Write_Clock.s
+++ source/Write_Clock.s
@@ -7,12 +7,10 @@
 
 // @param r0: The value to write.
 Write_Clock:
-	push {r1, r2, r3}
 	ldr r2, =0x20200000 //Base GPIO reg
 	mov r3, #1 //0b001
 	lsl r3, #11 //align for appropriate pin
 	teq r1, #0
 	streq r3, [r2, #40] //GPCLR0
 	strne r3, [r2, #28] //GPSET0
-	pop {r1, r2, r3}
 	bx lr
