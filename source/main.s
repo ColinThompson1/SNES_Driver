1becad3e2dd1b0bd03d8264fea2c0235b7c0d0a7
Colin Thompson <colinthompson1111@gmail.com>
2016-03-09 20:40:18 -0700

    Update main.s

--- source/main.s
+++ source/main.s
@@ -12,51 +12,67 @@ _start:
 .section .text
 
 main:
-    mov     sp, #0x8000
+    	mov     sp, #0x8000
+	
+	bl	EnableJTAG
+	bl	InitUART
+	//Initialize gpio lines
+	mov	r0, #9
+	ldr	r1, =20200000
+	bl	Init_GPIO
 
-	bl		EnableJTAG
-	bl 		Read_SNES
+	mov	r0, #10
+	ldr	r1, =20200004
+	bl	Init_GPIO
+
+	mov	r0, #11
+	ldr	r1, =20200004
+	bl	Init_GPIO
 
 	// Print names of creators
 	mov 	r0, #13
-	bl 		Print_Message
+	bl 	Print_Message
 
 Begin:
 
 	// Prompt for button press
-	mov		r0,	#14
-	bl 		Print_Message
+	mov	r0,	#14
+	push {lr}
+	bl 	Print_Message
+	pop {lr}
 
 TryAgain:
 	// Check for checked buttons. Bitmap returned in r0
-	bl		Read_SNES
+	bl	Read_SNES2
 
 	// Initiate counter to 0
-  // Mask to left most bit
-  // copy mask from Read_SNES to r7
-	mov		r4, #0
+  	// Mask to left most bit
+  	// copy mask from Read_SNES to r7
+	mov	r4, #16
 	mov 	r5, #1
-	mov		r7, r0
+	mov	r7, r0
 
-  // Loop through each bit in mask and check if that bit's corresponding button
-  // was pressed.
+  	// Loop through each bit in mask and check if that bit's corresponding button
+  	// was pressed.
 	Loop:
-		and		r6, r5, r7       // Isolate for left most bit
-		cmp		r6, r5           // See if leftmost bit is selected
-		beq		success
+		and	r6, r5, r7       // Isolate for left most bit
+		cmp	r6, #0           // See if leftmost bit is selected
+		beq	success
 
 		lsr	r7, #1             // Move in next bit to check.
-		add	r4, #1             // Inc counter
-		cmp r4, #16            // Check if its seen all buttons.
-		ble	Loop               // If there are more buttons to check restart.
+		sub	r4, #1             // Inc counter
+		cmp 	r4, #0             // Check if its seen all buttons.
+		bge	Loop               // If there are more buttons to check restart.
 
-    b TryAgain             // No button was pressed. Retry.
+    		b TryAgain             // No button was pressed. Retry.
 
 
     success:
-      mov		r0, r4         // If button is selected print the button type.
-		  bl		Print_Message
-      b Begin              // Restart input.
+      	mov	r0, r4         		// If button is selected print the button type.
+	push {lr}
+	bl	Print_Message
+	pop {lr}
+      	b Begin              		// Restart input.
 
 
 
